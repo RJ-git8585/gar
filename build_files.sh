2dd8af748386d82fc58b117fc28b30a82f09741b
Ritik Jaiswal <ritikjaiswal8888@gmail.com>
2024-05-15 17:25:45 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 pip install -r requirements.txt 
 Python 3.12.3 manage.py collectstatic
 
+$PIP_PATH install -r requirements.txt
+$PYTHON_PATH manage.py migrate
+$PYTHON_PATH manage.py collectstatic --noinput
